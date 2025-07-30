e09c7463f0462f176233b93115e70fd309737d02
Scott Westover <scottwestover2006@gmail.com>
2025-07-29 20:05:39 -0400

    changed: updated template to use eslint v9
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- config/eslint.config.mjs
+++ config/eslint.config.mjs
@@ -1,31 +1,20 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfig from "@devshareacademy/eslint-config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
+export default [
+  {
     ignores: ["**/node_modules", "**/dist"],
-}, ...compat.extends("@devshareacademy/eslint-config"), {
+  },
+  ...eslintConfig,
+  {
     languageOptions: {
-        ecmaVersion: 5,
-        sourceType: "script",
-
-        parserOptions: {
-            project: "./tsconfig.json",
-        },
+      ecmaVersion: 5,
+      sourceType: "script",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
     },
-
     rules: {
-        "@typescript-eslint/unbound-method": "off",
+      "@typescript-eslint/unbound-method": "off",
     },
-
-    files: ["**/*.ts", "**/*.tsx"]
-}];
+  },
+];
